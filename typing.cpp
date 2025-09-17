c01cd96c0a86f8ba9bfb18492e0cdbc8b3fde29c
tanmoyPaul12 <tanmoy01paul@gmail.com>
2025-09-18 00:14:48 +0530

    done

--- typing.cpp
+++ typing.cpp
@@ -15,18 +15,36 @@ int main(){
     "You get what you work for, not what you wish for.",
     "The quick brown fox jumps over the lazy dog.",
     "practice makes perfect.",
-    "Don’t watch the clock; do what it does. Keep going.",
+    "Donot watch the clock; do what it does. Keep going.",
     "Where focus goes, energy flows.",
-    "Discipline is doing what needs to be done, even when you don’t feel like doing it."
+    "Discipline is doing what needs to be done, even when you donot feel like doing it."
     } ;
 
     int total = sizeof(sentences)/sizeof(sentences[0]);
     int random = rand() % total ; // a random index of sentence we will get
     string sentence = sentences[random];
+    cout<<sentence<<endl;
+
+    time_t start = time(NULL);
+    string input;
+    // cout<<input<<endl;
+    getline(cin, input);
     
+    time_t end = time(NULL);
+
+    double timeUsed = difftime(end,start);
+
+    int words = 1;
+    for (int i = 0; i<input.length(); i++){
+        if(input[i]== ' '){
+            words++;
+        }
+    }
+                                    // speed in min
+    int wpm = (words/timeUsed)*60;
     
-    
-    
+    cout<<"Time Required: "<<timeUsed<<" seconds"<<endl;
+    cout<<"Speed: "<<wpm<<" WPM"<<endl;
     
     
     
